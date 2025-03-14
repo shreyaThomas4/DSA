aed209349c6fe1ca0f9763e6cf54ac9d909f3a49
shreya.thomas <shreya.thomas18@gmail.com>
2025-03-14 13:58:42 +0530

    find the longest string and the length of the string

--- Hashing/LongestSubString.js
+++ Hashing/LongestSubString.js
@@ -1,27 +1,32 @@
-var lengthOfLongestSubstring = function(s) {
-    let end = 0;
-    let start = 0;
-    let maxlength = 0;
+const lengthOfLongestSubstring = function (s) {
+  let end = 0;
+  let start = 0;
+  let maxlength = 0;
+  let longestSubstring = ""; 
 
-    if(!s) {
-        return 0;
-    }
+  if (!s) {
+    return "";
+  }
 
-const uniqueChar = new Set();
+  const uniqueChar = new Set();
 
-while(end< s.length) {
-    if(!uniqueChar.has(s[end])){
-       uniqueChar.add(s[end]);
-       end++;
-       maxlength=Math.max(maxlength,uniqueChar.size) 
+  while (end < s.length) {
+    if (!uniqueChar.has(s[end])) {
+      uniqueChar.add(s[end]);
+      end++;
+      
+      // Update the longest substring if the current one is longer
+      if (uniqueChar.size > maxlength) {
+        maxlength = uniqueChar.size;
+        longestSubstring = s.slice(start, end);  // Store the substring from start to end
+      }
     } else {
-        uniqueChar.delete(s[start]);
-        start++;
+      uniqueChar.delete(s[start]);
+      start++;
     }
-}
-    return maxlength;
-
+  }
+  return {longestSubstring: longestSubstring, length: longestSubstring.length};
 };
 
-const ans = lengthOfLongestSubstring('abcabcbb');
-console.log("ans", ans);
+const ans = lengthOfLongestSubstring("abcabcbb");
+console.log("Longest substring:", ans);
