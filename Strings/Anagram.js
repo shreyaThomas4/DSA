2f265431ef2f28fde91fa61fa2c02d961c1ba684
shreya.thomas <shreya.thomas18@gmail.com>
2025-04-24 20:25:13 +0530

    Check if string are anagram

---
+++ Strings/Anagram.js
@@ -0,0 +1,24 @@
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+
+function isAnagram(s, t) {
+    if(s.length !== t.length) return false;
+
+    const count = {};
+
+    for(let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for(let char of t) {
+        if(!count[char]) {
+            return false; 
+        }
+        count[char]--; 
+    }
+    return true;
+};
+
+console.log(isAnagram("anagram", "nagaram")); // true
+console.log(isAnagram("rat", "car"));         // false
